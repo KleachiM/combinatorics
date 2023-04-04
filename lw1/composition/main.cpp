0f34be6ce3b235c447dfd40efecd0bd09a2b16d3
Михаил Калинин <65865966+KleachiM@users.noreply.github.com>
2023-04-04 12:56:57 +0300

    доделал композицию положительного числа

--- lw1/composition/main.cpp
+++ lw1/composition/main.cpp
@@ -4,106 +4,86 @@
 #include <algorithm>
 using namespace std;
 
-vector<int> GetVector(const int count)
+void PrintComposition(const int number, const vector<int>& composition)
 {
-    vector<int> vec(count);
-    if (count < 1) return vec;
-    vec[0] = -1;
-    for (int i = 1; i < count + 1; i++)
-    {
-        vec[i] = i;
-    }
-    return vec;
+    cout << number << " = ";
+    copy(composition.begin(), composition.end() - 1, ostream_iterator<int>(cout, " + "));
+    copy(composition.end() - 1, composition.end(), ostream_iterator<int>(cout, "\n"));
 }
 
-/*
+vector<int> GetComposition(const vector<int>& delimiters, int elemsCount, int delimitersCount)
+{
+    vector<int> compositionsVec;
+    if (delimitersCount == 0)
+    {
+        compositionsVec.push_back(elemsCount); // для случая 3 = 3
+        return compositionsVec;
+    }
+    if (delimiters[delimitersCount - 1] > elemsCount - 2)
+    {
+        return compositionsVec;
+    }
+    for (int i = 0; i < delimitersCount; i++)
+    {
+        if (i == 0)
+        {
+            compositionsVec.push_back(delimiters[i]);
+            continue;
+        }
+        int n = delimiters[i] - delimiters[i - 1] - 1;
+        if (n > 0)
+        {
+            compositionsVec.push_back(n);
+        }
+        else
+        {
+            compositionsVec.clear();
+            break;
+        }
+    }
+    if (compositionsVec.size() > 0)
+    {
+        int lastN = elemsCount - delimiters[delimitersCount - 1] - 1;
+        if (lastN > 0)
+        {
+            compositionsVec.push_back(lastN);
+            return compositionsVec;
+        }
+        compositionsVec.clear();
+    }
+    return compositionsVec;
+}
+
 int main()
 {
+    cout << "Введите целое положительное число: ";
     int n;
-    cout << "Введите целое положительное число больше 0" << endl;
     cin >> n;
-
-    if (n == 1)
+    for (int k = 0; k < n; k++)
     {
-        cout << "1 = 1" << endl;
-    }
+        int elemsCount = n + k, delimitersCount = k;
+        vector<int> v(delimitersCount + 1);
+        v[0] = 0;
+        for(int i = 1; i < v.size(); i++)
+        {
+            v[i] = i;
+        }
 
-    for (int k = 1; k < n; k++)
-    {
-        vector<int> v = GetVector(k);
         int m = 1;
         while (m != 0)
         {
-            if (n == accumulate(v.begin() + 1, v.end(), 0))
+            vector<int> delimitersPositions;
+            copy(v.begin() + 1, v.end(), back_inserter(delimitersPositions));
+            vector<int> composition = GetComposition(delimitersPositions, elemsCount, delimitersCount);
+            if (composition.size() > 0)
             {
-                cout << n << " = ";
-                copy(v.begin() + 1, v.end() - 1, ostream_iterator<int>(cout, " + "));
-                copy(v.end() - 1, v.end(), ostream_iterator<int>(cout));
-                cout << endl;
+                PrintComposition(n, composition);
             }
-            m = k;
-            while (v[m] == n - k + m) m--;
+            m = delimitersCount;
+            while (v[m] == elemsCount - delimitersCount + m) m--;
             v[m]++;
-            for(int i = m + 1; i < v.size(); i++)
-            {
-                v[i] = v[i - 1] + 1;
-            }
+            for(int i = m + 1; i < v.size(); i++) v[i] = v[i - 1] + 1;
         }
     }
-  */
-/* int n = 3,k = 3;
-   vector<int> v(k);
-   for(int i = 0; i < k; i++)
-   {
-       v[i] = 1;
-   }
-   while(v[k] != 1)
-   {
-       reverse_copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
-       cout << endl;
-       int i = 0;
-       while (v[i] == n)
-       {
-           v[i] = 1;
-           i++;
-       }
-       v[i]++;
-   }*//*
 
-
-}*/
-
-vector<int> GetCompositions(const vector<int>& delimiters, int elemsCount, int delimitersCount)
-{
-    vector<int> compositionsVec(delimitersCount + 1);
-
-    return vector<int>;
-}
-
-int main()
-{
-    int n = 5, k = 2;
-    vector<int> v(k + 1);
-    v[0] = 0;
-    for(int i = 1; i < v.size(); i++)
-    {
-        v[i] = i;
-    }
-
-    int m = 1;
-    while (m != 0)
-    {
-        vector<int> delimiters;
-        copy(v.begin() + 1, v.end(), back_inserter(delimiters));
-        vector<int> compositions = GetCompositions(delimiters, n, k);
-        if (compositions.size() > 0)
-        {
-            copy(compositions.begin(), compositions.end(), ostream_iterator<int>(cout, " "));
-            cout << endl;
-        }
-        m = k;
-        while (v[m] == n - k + m) m--;
-        v[m]++;
-        for(int i = m + 1; i < v.size(); i++) v[i] = v[i - 1] + 1;
-    }
 }
